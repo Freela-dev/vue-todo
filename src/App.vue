3a231e903352831ca3616506e016f238c62133e3
rdrgzma <rdrgzma@hotmail.com>
2021-08-14 18:33:14 +0000

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -37,10 +37,40 @@
               </v-list>
       </v-navigation-drawer>
 
-      <v-app-bar app>
-          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-          <v-toolbar-title>Todo</v-toolbar-title>
-      </v-app-bar>
+     
+         <v-app-bar
+     app
+      color="#fcb69f"
+      dark
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>ToDo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+     
 
       <v-main>
           <router-view/>
@@ -54,8 +84,8 @@
     data: () => ({ 
       drawer: null,
        items: [
-        {title: 'Tarefas', icon: 'mdi-view-dashboard', to:'/'},
-        {title: 'Sobre', icon: 'mdi-help-box', to: '/sobre'},
+        {title: 'Tarefas', icon: 'mdi-briefcase-edit-outline', to:'/'},
+        {title: 'Sobre', icon: 'mdi-contacts-outline', to: '/sobre'},
       ]
     })  
   }
