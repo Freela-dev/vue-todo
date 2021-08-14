0c9d17b56574b92799d0b6ca70f34bbb131dde26
rdrgzma <rdrgzma@hotmail.com>
2021-08-14 16:32:34 +0000

    add layout and fix hot reload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    public: 'http://localhost:8080',
+    watchOptions: {
+      ignored: /node_modules/,
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  }
 }
